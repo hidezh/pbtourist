67d2650ce758ea285adeb263c8092a74e19018a2
yoshikizh <177365340@qq.com>
2013-02-26 07:35:50 -0800

    create proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pbtourist::Application.config.secret_token = 'c6aa84ccb54245333abe82e5b4ea99c2b387581957ee45b46f7f9095918943a983209958d094df472cbebab5bcac167d06217ef55869da3d7c32b45f3d192fa2'
