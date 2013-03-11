838e4911298029aff123d871e9ef43bc5a56af7f
yoshikizh <177365340@qq.com>
2013-03-11 01:56:09 -0700

    modify username validate

--- app/models/user_soon.rb
+++ app/models/user_soon.rb
@@ -7,7 +7,7 @@ class UserSoon < ActiveRecord::Base
   validates_length_of     :email, maximum: 32 , message: "EMAIL不得超过32个字符 | Email is no more than 32 letters"
 
   validates_presence_of   :name,  message: "很抱歉，请填写邮箱信息 | The Email appears to be missing"
-  validates_length_of     :name,  within:  4..32, :too_long => "请输入一个较短的用户名 | Please enter a shorter name",
+  validates_length_of     :name,  within:  2..32, :too_long => "请输入一个较短的用户名 | Please enter a shorter name",
                                                   :too_short => "请输入一个较长的用户 | pick a longer name"
   validates_uniqueness_of :name,  message: "此用户名已被使用 | The Username has been occupied"
   # validates :name, uniqueness: true, presence: true,:length => { :maximum => 30 }
